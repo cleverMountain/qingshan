e420ab2772c3eae44850f62cc1c3e4bd129ac7b0
Hjl <1031939925@qq.com>
2023-08-29 17:40:51 +0800

    菜单提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,61 +1,65 @@
 // import sidebarArr from './config/sidebar'
+const path = require('path')
 module.exports = {
-  title: 'note',
+  title: '青山的博客',
   description: 'Just a note about js',
   base: '/qingshan/',
+  stylus: {
+    import: [
+      path.resolve(__dirname, './styles/index.styl')
+    ]
+  },
   themeConfig: {
-    // sidebar: {
-    //   '/foo/': [
-    //     '',     /* /foo/ */
-    //     'one',  /* /foo/one.html */
-    //     'two'   /* /foo/two.html */
-    //   ],
-
-    //   '/bar/': [
-    //     '',      /* /bar/ */
-    //     'three', /* /bar/three.html */
-    //     'four'   /* /bar/four.html */
-    //   ],
-    // },
-    // sidebar: {
-    //   '/guide/': [
-    //     '',        // 对应 guide 文件夹的 README.md 文件
-    //     'getting-started',
-    //     'configuration'
-    //     // 添加更多的侧边栏链接
-    //   ]
-    // },
-    sidebar: {
-      '/guide/': [{
-        title: 'js基础',
-        collapsable: true, // 是否能收起来
-        children: [
-          'configuration',
-          'getting-started'
-        ]
-      }],
-      '/': ['/guide/']
-    },
     nav: [
-      { text: '指南', link: '/guid312e1' },
-      { text: 'API 文档', link: '/api1' },
-      { text: 'GitHub', link: 'https://github.com/your-repo' }
+      {
+        text: "主页",
+        link: "/",
+      },
+      {
+        text: "基础知识",
+        link: "/base/",
+      },
     ],
 
+    sidebar: {
+      '/base/': [
+        {
+          title: '基础知识',
+
+          children: [
+            {
+              title: "js基础",
+              link: "/base/jsBasic/",
+              children: [
+                {
+                  title:"关于数组方法",
+                  path: '/base/jsBasic/array/'
+                },
+                {
+                  title:"关于对象方法",
+                  path: '/base/jsBasic/object/'
+                },
+              ]
+            },
+            {
+              title: "CSS",
+              link: "/articles/CSS/",
+            },
+          ]
+        },
+        // {
+        //   title: "HTML",
+        //   link: "/articles/HTML/",
+        // },
+        // {
+        //   title: "CSS",
+        //   link: "/articles/CSS/",
+        // },
+      ],
+
+    },
+
+
+
   }
-  // themeConfig: {
-  //   nav: [
-  //     { text: '首页', link: '/' },
-  //     { text: '关于', link: '/about/' },
-  //     // 添加更多的导航链接
-  //   ],
-  //   sidebar: {
-  //     '/guide/': [
-  //       '',        // 对应 guide 文件夹的 README.md 文件
-  //       'getting-started',
-  //       'configuration'
-  //       // 添加更多的侧边栏链接
-  //     ]
-  //   }
-  // }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,16 @@
-# 231wewqeewq22ew
-```js
-let a = 1
-```
+---
+home: true
+heroImage: /img/hero.png
+heroText: 青山的博客
+tagline: 种一棵树最好的时间在十年前，其次是现在。
+actionText: 开始阅读 →
+actionLink: /base/
+features:
+  - title: 技术文章1
+    details: 博文以JavaScript、Vue、React、小程序、Node等技术文章为主。
+  - title: 计算机通识
+    details: 作为是一个软件工程师，计算机基础知识必不可少。
+  - title: 生活杂文
+    details: 随手记录一些生活、学习、工作感悟，偶尔引用一些好文章。
+footer: MIT Licensed | Copyright © 2018-2023 AnsonZnl
+---
